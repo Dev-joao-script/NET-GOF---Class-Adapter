4430f7ae9462bc4b3c31c1f80d71a3471052f75b
Dev-joao-script <Dev.joao.script@gmail.com>
2022-10-28 18:17:22 -0200

    Revisão de conteudo

---
+++ Adapter1/ICalculaMensalidade.cs
@@ -0,0 +1,8 @@
+﻿namespace Adapter1
+{
+    //Target
+    public interface ICalculaMensalidade
+    {
+        void ProcessaCalculoMensalidade(string[,] alunosArray);
+    }
+}
---
+++ Adapter1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Adapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var alunosArray = SistemaEscolar.GetListaAlunosMensalidades();
+
+            ICalculaMensalidade calculo = new AlunosAdapter();
+
+            calculo.ProcessaCalculoMensalidade(alunosArray);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
